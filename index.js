6267a507bb4ffe8e02ab3946400cc7b0cf5c5e11
uccuz <yhlin.cs11@nycu.edu.tw>
2023-07-29 23:47:03 +0800

    feat: simple web app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: '2023 COSCUP Demo Failed!!' });
+  //res.render('index', { title: '2023 COSCUP Demo Succeeded!!' });
+});
+
+app.listen(8080, () => {
+  console.log('Server started on port 8080');
+});
