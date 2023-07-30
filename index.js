c02e67e42ab68a2e42cee871e5c5a49e9be371d5
uccuz <yhlin.cs11@nycu.edu.tw>
2023-07-30 13:34:16 +0800

    chore: update title

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  //res.render('index', { title: '2023 COSCUP Demo Failed!!' });
-  res.render('index', { title: '2023 COSCUP Demo Succeeded!!' });
+  res.render('index', { title: '2023 COSCUP Demo Failed!!' });
+  //res.render('index', { title: '2023 COSCUP Demo Succeeded!!' });
 });
 
 app.listen(8080, () => {
